6b9658e6c620f5a8e3dd288bf8250b39cbac2ef3
Chandan Gowda K S <chanduchess122@gmail.com>
2025-07-19 01:54:59 +0530

    Add files via upload

---
+++ JavaScript_Day_3,4/app.js
@@ -0,0 +1,29 @@
+// let dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+// Qa2
+// Create an object representing a car that stores the following
+// properties for the car:name,model,color.Print the car’s name
+
+// const object = {
+//     Car_name: "Tata",
+//     model: "Gotilla",
+//     color : "White"
+// }
+
+// console.log(object);
+// console.log(object.Car_name);
+
+//Q3
+// Create an object Person with their name, age and city.
+// Edit their city’s original value to change it to “NewYork”.Add a new property country and set it to the UnitedStates.
+
+let person = {
+    name: "chandu",
+    age: 20,
+    city: "Bengaluru"
+}
+console.log(person);
+person.city = "New York";
+person.country = "USA";
+console.log(person);
