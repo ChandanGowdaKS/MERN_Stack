6eff99d611dfb32318b0a5c1e515bb8f70a88317
Chandan Gowda K S <chanduchess122@gmail.com>
2025-07-20 00:06:26 +0530

    Add files via upload

---
+++ JavaScript_Day_5/app.js
@@ -0,0 +1,123 @@
+// Concatenation using function
+
+// let array = ["Hello", "Chandu", "Theju", "Akka"];
+
+// function concate(array) {
+//     let final_string = "";
+//     for (char of array) {
+//         final_string += char;
+//     }
+//     return final_string;
+
+// }
+// let result = concate(array);
+// console.log(result);
+
+// QA1
+// Write a JavaScript function that returns array elements larger than a number
+
+// let arr = [2, 4, 6, 9];
+// let n = 4;
+// function larger_number(arr, n) {
+//     let final_array = [];
+//     for (i of arr) {
+//         if (i > n){
+//             final_array.push(i);
+//         }
+//     }
+//     return final_array;
+// }
+// console.log(larger_number(arr, n));
+
+// QA2
+// Write a JavaScript function to extract unique characters from a string.
+// Example:str=“abcdabcdefgggh” ans=“abcdefgh”
+
+// let str = "chandu chess super man";
+
+// function unique(str) {
+//     let final_str = "";
+//     for (char of str) {
+//         if (final_str.includes(char) == false) {
+//             final_str += char;
+//         }
+//     }
+//     return final_str;
+// }
+// console.log(unique(str));
+
+// Q3
+// Write a JavaScript function that accepts a list of country names as input and returns the
+// longest country name as output.
+// Example:country=["Australia","Germany","United States of America"] output:"United States of America
+
+// let country = ["Australia", "Germany", "United States of America"];
+
+// function longest_country(country) {
+//     let highest = "";
+//     for (let i = 0; i < country.length; i++){
+//         if (country[i].length > highest.length) {
+//             highest = country[i];
+//         }
+//     }
+//     return highest;
+// };
+// console.log(longest_country(country));
+
+// Qa4
+// Write a JavaScript function to count the number of vowels in a String argument
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//QA5
+// Write a JavaScript function to generate a random number with in a range(start,end).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
