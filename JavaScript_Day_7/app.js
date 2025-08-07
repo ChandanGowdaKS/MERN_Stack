74f4e16b666bd2f230b2a4a54a50c23928b932b0
Chandan Gowda K S <chanduchess122@gmail.com>
2025-08-07 23:48:36 +0530

    Add files via upload

---
+++ JavaScript_Day_7/app.js
@@ -0,0 +1,20 @@
+// let array = [5, 6, 8, 9, 2, 11];
+// let arrayAverage = (array) => {
+//     let count = 0;
+//     let sum = 0;
+//     for (num of array) {
+//         count++;
+//         sum += num;
+//     }
+//     return (sum/count);
+// }
+// console.log(arrayAverage(array));
+
+// let isEven = (n) => {
+//     if (n % 2 == 0) {
+//         console.log("Even");
+//     } else {
+//         console.log("Odd");
+//     }
+// }
+// console.log(isEven(11));
---
+++ JavaScript_Day_8/app.js
@@ -0,0 +1,22 @@
+// let arr = [1, 2, 3, 4, 5];
+
+// let square = arr.map((num) => {
+//     return num * num;
+// });
+// let sum = arr.reduce((num, num1) => {
+//     return num + num1;
+// });
+// console.log(square);
+
+// let avg = sum/arr.length;
+// console.log(avg);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let final = arr.map((ele) =>
+//     ele + 5
+// );
+// console.log(final);
+
+let string = ["hello", "chandu", "how", "are", "you"];
+console.log(string.map((string) => string.toUpperCase()));
