2cebb08e61f0714834c68af69fd685aa0488ccc5
Chandan Gowda K S <chanduchess122@gmail.com>
2025-10-09 22:24:05 +0530

    Initial Commit

--- Day_5REST_API/index.js
+++ Day_5REST_API/index.js
@@ -7,10 +7,37 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("Server is working!!!")
+let posts = [
+    {
+    username: "Chandan",
+    Content: "International Rated chess Player and Software Developer",
+},
+    {
+    username: "Shiva",
+    Content: "God",
+},
+    {
+    username: "BMS",
+    Content: "College at Bengaluru",
+    },
+];
+
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs",{posts});
+    // res.send("Server is working");
+})
+
+app.get("/posts/new", (req, res) => {
+    res.render("new_post.ejs");
+})
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    posts.push({ username, Content: content }); // Correct way to add
+    res.redirect("/posts"); // Redirect to main page
 })
 
 app.listen(port, () => {
