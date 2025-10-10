74348611d68a9d153795e8ed281445f1a994a44b
Chandan Gowda K S <chanduchess122@gmail.com>
2025-10-10 22:53:41 +0530

    Initial commit

--- Day_5REST_API/index.js
+++ Day_5REST_API/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,14 +12,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+    id:uuidv4(),
     username: "Chandan",
     Content: "International Rated chess Player and Software Developer",
 },
     {
+    id:uuidv4(),
     username: "Shiva",
     Content: "God",
 },
     {
+    id:uuidv4(),
     username: "BMS",
     Content: "College at Bengaluru",
     },
@@ -35,9 +39,26 @@ app.get("/posts/new", (req, res) => {
 })
 
 app.post("/posts", (req, res) => {
+    let id = uuidv4();
     let { username, content } = req.body;
-    posts.push({ username, Content: content }); // Correct way to add
+    posts.push({ id,username, Content: content }); // Correct way to add
     res.redirect("/posts"); // Redirect to main page
+})
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    console.log(id);
+    let post = posts.find((p) => id === p.id);
+    res.render("show", { post });
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    let newContent = req.body.content;
+    post.Content = newContent;
+    console.log(post);
+    res.send("patch request is working");
 })
 
 app.listen(port, () => {
