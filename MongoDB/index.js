e21eec754de5fa42168088c8edb89e929813fda3
Chandan Gowda K S <chanduchess122@gmail.com>
2026-01-03 19:12:22 +0530

    Initial commit

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -22,15 +22,55 @@ const userSchema = mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 // inserting
 
-const user1 = new User({
-    name: "chandu",
-    email: "cha@gmail.com",
-    age: 20,
+// const user1 = new User({
+//     name: "chandu",
+//     email: "cha@gmail.com",
+//     age: 20,
     
-});
+// });
 
-user1.save().then((res) => {
-    console.log(res);
-}).catch(err => {
-    console.log(err);
-});
+// user1.save().then((res) => {
+//     console.log(res);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// inserting many collections
+
+// User.insertMany([
+//     { name: "chan", email: "gowda@gmailcom", age: 20 },
+//     { name: "god", email: "god@gmail", age: 25 },
+//     { name: "shiva", email: "shiva@gmail.com", age: 30 },
+// ]).then((res) => {
+//     console.log(res);
+// });
+
+// to find in mongoose
+// User.find().then((res) => {
+//     console.log(res);
+// })
+
+// User.find({ age: { $gt: 20 } }).then((data) => {
+//     console.log(data);
+// });
+
+// update
+// User.updateOne({ name: "chan" }, { age: 50 }).then((data) => {
+//     console.log(data);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// updating many
+// User.updateMany({ age: { $lt: 40 } }, { age: 60 }).then((data) => {
+//     console.log(data);
+// });
+
+// User.find().then((data) => {
+//     console.log(data);
+// });
+
+//delete
+// User.findOneAndDelete({ name: "chan" }).then((data) => {
+//     console.log(data);
+// });
